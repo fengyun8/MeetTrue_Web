1a726fe9cf3adb09822b5f3eca2213aaeb217e00
leenty <leenty@qq.com>
2016-06-28 15:52:52 +0800

    added browserSync

--- Code/gulpfile.js
+++ Code/gulpfile.js
@@ -1,17 +1,9 @@
 var elixir = require('laravel-elixir');
-
 require('laravel-elixir-stylus');
 
 elixir(function(mix) {
-   mix.stylus('app.styl')
-      .browserify('app.js');
-});
-
-elixir(function(mix) {
-   mix.stylus('admin.styl')
-      .browserify('admin.js');
-});
-
-elixir(function(mix) {
-   mix.stylus('admin.styl');
+  // mix.browserSync({
+  //     proxy: "mt.dev"
+  //   })
+  mix.stylus(['app.styl', 'admin.styl']);
 });
