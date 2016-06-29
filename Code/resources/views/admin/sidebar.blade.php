a32b2278c90316b1e684a315462a20a74bd003ba
zhanyulong <zhanyuilong@gmail.com>
2016-06-29 10:12:34 +0800

    全局样式修改（添加链接，字体，变量）&& admin 样式布局

--- Code/resources/views/admin/sidebar.blade.php
+++ Code/resources/views/admin/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
   <ul class="sidebar__list">
     <li>
-      <a class="sidebar__link sidebar__link--active" href="/admin/stories">专题</a>
+      <a class="sidebar__link sidebar__link--active" href="#">专题</a>
     </li>
   </ul>
 </div>
