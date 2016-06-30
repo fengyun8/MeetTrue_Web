21c2e6c3db7d449e5b174c60397b911657d34876
leenty <leenty@qq.com>
2016-06-30 13:15:40 +0800

    1.css button和input的class完善 2.css flex布局的mixins改成传参方式

--- Code/resources/views/admin/activity.blade.php
+++ Code/resources/views/admin/activity.blade.php
@@ -7,8 +7,8 @@
     </p>
     <div class="operatingFloor">
       <p class="operatingFloor__titleBar">
-        <span class="operatingFloor__title">banner</span>
-        <span class="operatingFloor__title">链接</span>
+        <span class="operatingFloor__title">活动图片</span>
+        <span class="operatingFloor__title">信息</span>
         <span class="operatingFloor__title">操作</span>
       </p>
       <p class="operatingFloor__itemBar">
@@ -16,7 +16,16 @@
           <img src="http://images.meet-true.com/default/201605/kunjugn1pxvc9aqt.jpg@980w_456h" alt="">
         </span>
         <span class="operatingFloor__item">
-          <input class="input" type="text" placeholder="输入链接地址">
+          <input class="input" type="text" placeholder="输入活动标题">
+          <input class="input" type="text" placeholder="输入活动简介">
+          <span class="btn__group">
+            <span class="input__addon">开始</span>
+            <input type="date" class="input" >
+          </span>
+          <span class="btn__group">
+            <span class="input__addon">结束</span>
+            <input type="date" class="input">
+          </span>
         </span>
         <span class="operatingFloor__item">
           <button class="operatingFloor__btn btn__noStyle">删除此项</button>
