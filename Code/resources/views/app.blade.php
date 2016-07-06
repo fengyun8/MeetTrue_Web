3341e8dd4774ad20ca9106dfd64c9c1dfac5d003
leenty <leenty@qq.com>
2016-07-06 17:28:31 +0800

    下拉选择框UI组件第一版，使用class：btn__select调用，后面会写它的js

--- Code/resources/views/app.blade.php
+++ Code/resources/views/app.blade.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>@yield('title') | 觅处 | Meet-True</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
-    </head>
-    <body class="@yield('body-class')" data-module="search @yield('module')">
-        @include('svg.all')
-        @include('shared.nav')
-        @yield('below-nav')
-        <div class="content" id="app">
-            <div class="surface">
-                @yield('content')
-            </div>
-        </div>
-        @include('shared.footer')
-    <script src="{{ asset('js/app.js')}}"></script>
-  </body>
+  <head>
+    <title>@yield('title') | 觅处 | Meet-True</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+  </head>
+  <body class="@yield('body-class')" data-module="search @yield('module')">
+    @include('svg.all')
+    @include('shared.nav')
+    @yield('below-nav')
+    <div class="content" id="app">
+      <div class="surface">
+        @yield('content')
+        <!-- <span class="btn__select">
+          <input type="text" value="123" placeholder="专业">
+          <button type="button"></button>
+          <ul>
+            <li>123</li>
+            <li>456</li>
+            <li>789</li>
+          </ul>
+        </span> -->
+      </div>
+    </div>
+    @include('shared.footer')
+  <script src="{{ asset('js/app.js')}}"></script>
+</body>
 </html>
